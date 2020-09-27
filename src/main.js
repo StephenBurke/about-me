8daf183b1a729edd40fd957ec68128a0fd2ca002
Stephen Burke <burke.stephenpaul@gmail.com>
2020-09-27 18:06:04 -0500

    Logic issues resolved

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 const multiply = (x, y, z) => {
   return x * y * z;
 };
-
+/*
 const validate = async (event) => {
   console.log(`triggered validate on ${event.target.id}`);
   const isValid = event.target.checkValidity();
@@ -12,10 +12,10 @@ const validate = async (event) => {
     event.target.focus();
   }
 };
-
+*/
 const updateWithMultiply = async (event) => {
   try {
-    document.querySelector("#result").innerHTML = "";
+    //document.querySelector("#result").innerHTML = "";
     if (
       document.querySelector("#length").checkValidity() &&
       document.querySelector("#width").checkValidity() &&
@@ -29,25 +29,14 @@ const updateWithMultiply = async (event) => {
       const i = parseInt(document.querySelector("#length").value);
       const j = parseInt(document.querySelector("#width").value);
       const k = parseInt(document.querySelector("#height").value);
-      const ans = `$Your volume is ${multiply(i, j, k)}.`;
+      const ans = `Your volume is ${multiply(i, j, k)}.`;
       document.querySelector("#result").innerHTML = ans;
+      return ans;
     }
   } catch (error) {
     console.error(error);
   }
 };
-
-// delegate to dynamic elements (e.g. when testing)
-// focusout is like blur, but it bubbles up
-document.addEventListener("focusout", (event) => {
-  if (
-    (event.target && event.target.id === "length") ||
-    (event.target && event.target.id === "width") ||
-    (event.target && event.target.id === "height")
-  ) {
-    validate(event);
-  }
-});
 
 document.addEventListener("click", (event) => {
   if (event.target && event.target.id === "multiplyBtn") {
